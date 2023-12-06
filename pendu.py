1ad92cef02dab3eda431b0ac1b1c54692a3b8741
jjlefrant <149365674+jjlefrant@users.noreply.github.com>
2023-12-06 14:42:03 +0100

    version corrigée

--- pendu.py
+++ pendu.py
@@ -1,6 +1,6 @@
 def pendu():
     """
-    jeu du pendu en 8 tentatives
+    jeu du pendu en 6 tentatives
     """
     nb_lettres_trouvees = 0
     nb_essais=0
@@ -10,28 +10,30 @@ def pendu():
     for i in range(25):
         print()
 
-    tabLettresTrouvees="_"*len(tabMotATrouver)
+    l=len(tabMotATrouver)
+    tabLettresTrouvees="_"*l
     
     print(tabLettresTrouvees)
 
-    while nb_lettres_trouvees<len(tabMotATrouver) and nb_essais<=7:
+    while nb_lettres_trouvees<l and nb_essais<6:
         lettre = str(input("Entrez une lettre : "))
         trouve=False
-        for i in range(len(tabMotATrouver)):
-            if tabMotATrouver[i]==lettre[0]:
+        for i in range(l):
+            if tabMotATrouver[i]==lettre[0] and tabMotATrouver[i]!=tabLettresTrouvees[i]:
                 tabLettresTrouvees=tabLettresTrouvees[:i]+tabMotATrouver[i]+tabLettresTrouvees[i+1:]
                 nb_lettres_trouvees+=1
                 trouve=True
-                if nb_lettres_trouvees==len(tabMotATrouver):
-                    print("Gagné !!!")
+                if nb_lettres_trouvees==l:
+                    print("Gagné (^.^)")
                     break
         print(tabLettresTrouvees)
         if not trouve:
             nb_essais+=1
-        print("Il vous reste : ", 7-nb_essais," tentatives")
+        print("Il vous reste : ", 6-nb_essais," tentatives")
 
     if not trouve:
-        print("Perdu !")
+        print("Perdu :(")
+        print("Le mot était :", tabMotATrouver)
     return trouve
 
 pendu()
